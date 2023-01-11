dfc1a8168dfbb799e240955bdb609e4b9e4caf5b
SashaKrynytska <krynytskaya@gmail.com>
2023-01-12 00:22:36 +0200

    update section.js

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,13 +1,6 @@
 import PropTypes from 'prop-types';
 import css from './Section.module.css';
 
-// const Section = ({ title, children }) => (
-//   <div className="SectionItem">
-//     <h2>{title}</h2>
-//     {children}
-//   </div>
-// );
-
 export const Section = ({ title, children }) => (
   <section className={css.section}>
     <h2>{title}</h2>
